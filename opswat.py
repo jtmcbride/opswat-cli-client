55f63e3ae9ab5ed27379f8e1afa8a5bef98c3c76
Joseph McBride <jmcbriddle@gmail.com>
2018-04-18 17:05:48 -0700

    add more error handling and make python 2 compatible

--- opswat.py
+++ opswat.py
@@ -21,37 +21,58 @@ class OPSWATFileUpload():
 
 	def calculate_file_hash(self, file):
 		self.current_file_contents = file.read()
+		self.file.close()
 		self.current_file_hash = hashlib.sha1(self.current_file_contents).hexdigest().upper()
 		return self.current_file_hash
 
 	def get_report_by_hash(self):
 		file_hash =  self.calculate_file_hash(self.file)
-		res = requests.get('https://api.metadefender.com/v2/hash/%s' % file_hash, headers=self.default_headers)
+		res = requests.get(
+			'https://api.metadefender.com/v2/hash/%s' % file_hash, 
+			headers=self.default_headers
+			)
 		return res
 
 	def upload_file(self):
-		headers = {**self.default_headers, 'filename': self.current_filename}
-		res = requests.post('https://api.metadefender.com/v2/file', data={'file': self.current_file_contents}, headers=headers)
+		headers = {'filename': self.current_filename}
+		headers.update(self.default_headers)
+		res = requests.post(
+			'https://api.metadefender.com/v2/file', 
+			data={'file': self.current_file_contents}, 
+			headers=headers
+			)
 		return res
 
 
 	def cli_file_upload(self):
-		self.current_filename = sys.argv[1]
+		args = sys.argv
+		self.current_filename = args[1]
+		wait_time = args[2] if len(args) > 2 else 5
 		self.file = open(self.current_filename, 'rb')
 		report = self.get_report_by_hash()
 		report_json = report.json()
 		if self.current_file_hash in report_json and report_json[self.current_file_hash] == 'Not Found':
 			scan_file = self.upload_file()
 			if scan_file.status_code >= 400:
-				raise Exception(scan_file.status_code)
+				raise Exception("%s: %s" % (scan_file.status_code, scan_file.reason))
 			else:
 				self.current_data_id = scan_file.json()['data_id']
 
-			report = requests.get('https://api.metadefender.com/v2/file/%s' % self.current_data_id, headers=self.default_headers)
-			import pdb; pdb.set_trace()
-			while report.json()['original_file']['progress_percentage'] != 100:
-				input("Report still in queue. Hit enter to retry...")
-				report = requests.get('https://api.metadefender.com/v2/file/%s' % self.current_data_id, headers=self.default_headers)
+			report = requests.get(
+				'https://api.metadefender.com/v2/file/%s' % self.current_data_id, 
+				headers=self.default_headers
+				)
+			status = report.status_code
+			while status < 400 and report.json()['original_file']['progress_percentage'] != 100:
+				print('Scan not complete. Retrying in %s seconds...' % wait_time)
+				time.sleep(wait_time)
+				report = requests.get(
+					'https://api.metadefender.com/v2/file/%s' % self.current_data_id,
+					headers=self.default_headers
+					)
+				status = report.status_code
+		if status >= 400:
+			raise Exception("%s: %s" % (status, report.reason))
 		report_json = report.json()
 		print(self.format_report(report_json))
 		return report_json
@@ -72,26 +93,7 @@ class OPSWATFileUpload():
 
 
 if __name__ == '__main__':
-
-	up = OPSWATFileUpload()
-	# up.current_file = open('requirements.txt', 'rb')
-	# r = up.get_report_by_hash()
-	# import pdb; pdb.set_trace()
-	# print(r.json())
-	up.cli_file_upload()
-	# up.current_filename = 'testing2.txt'
-	# up.file = open('testing.txt', 'rb')
-	# by_h = up.get_report_by_hash()
-	# print(by_h.json())
-	# by_up = up.upload_file()
-	# print(by_up.json())
-	# data_id = by_up.json()['data_id']
-	# i = 0
-	# while i < 10:
-	# 	i += 1
-	# 	pdb.set_trace()
-	# 	report = requests.get('https://api.metadefender.com/v2/file/%s' % data_id, headers=up.default_headers)
-	# 	print(report.json())
+	OPSWATFileUpload().cli_file_upload()
 
 
 
